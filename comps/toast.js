03283febbc2b3cd087f2bb8b055ccda3a7e719e1
Jeanos <a01016451@se14l0121-19.ad.bcit.ca>
2019-10-22 11:51:19 -0700

    Staged

--- comps/toast.js
+++ comps/toast.js
@@ -7,28 +7,31 @@ import {
 import styles from '../styles/toastStyles';
 
 function Toast() {
-    const [showToast, setShowToast] = useState("Toast");
-    // var txt = "Toast"
-    if (showToast == "Toast"){
-        <View>
-            <TouchableOpacity>
-                <Text
-                // onPress={txt = "You've moved the Toast up"}
-                onPress={()=>{
-                    setShowToast("You've moved the Toast Up")
-                }}
-                >
-                </Text>
-            </TouchableOpacity>
-        </View>
+    var ToastText = 'Toast'
+    const [showToast, setShowToast] = useState(ToastText);
+    const [btnToast, setToastText] = useState(false);
+
+
+    var TouchableView = (<TouchableOpacity onPress={()=>{setToastText(!btnToast)}}> 
+        <Text style={styles.toasty}>
+            {ToastText}
+        </Text>
+    </TouchableOpacity>)
+
+
+    if (btnToast === true){
+        TouchableView = (<TouchableOpacity onPress={()=>{setToastText(!btnToast)}}> 
+            <Text style={styles.toasty}>
+                You move the toast Down
+            </Text>
+        </TouchableOpacity>)
     }
+
+    // var txt = "Toast"
+   
     return (
     <View styles={styles.toastView}>
-        <TouchableOpacity> 
-            <Text style={styles.toasty}>
-                {showToast}
-                 </Text>
-                 </TouchableOpacity>
+        {TouchableView}
 
     </View>
     )
