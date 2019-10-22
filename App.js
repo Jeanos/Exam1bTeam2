a34d3d347ea8c906e9e4893bdefcec143f6d67a3
jean-jacques Johnson <jeanjacques.johnson@gmail.com>
2019-10-22 00:56:10 -0700

    Set up App
    Set up App

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {View,Text} from 'react-native';
+import mainStyle from './styles/mainStyle';
+
+
+function App() {
+  return (
+    <View style={mainStyle.comp}>
+      <Text>App Goes Here</Text>
+    </View>
+  );
+}
+
+
+export default App;
---
+++ comps/main.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {View,Text, ScrollView} from 'react-native';
+
+
+import mainStyle from './styles/mainStyle';
+
+
+
+function Main() {
+  return (
+    <View style={mainStyle.App}>
+     <Text>Main</Text>
+    </View>
+  );
+}
+
+
+export default Main;
