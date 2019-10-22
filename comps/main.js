93d390c590c7799271be2d8303fe1b8a9a818d70
LucaMattia <1970sRocknroll>
2019-10-22 09:36:17 -0700

    fixed the main

--- comps/main.js
+++ comps/main.js
@@ -1,26 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {View,Text, ScrollView} from 'react-native';
+import {View, Text} from 'react-native';
 
+function Main(){
+    return(
+        <View style={{flex:1}}>
 
-import mainStyle from './styles/mainStyle';
-
-
-
-function Main() {
-  return (
-    <View style={mainStyle.App}>
-     <Text>Main</Text>
-    </View>
-  );
+ <Text>Hello</Text>
+        </View>
+    );
 }
-
 
 export default Main;
